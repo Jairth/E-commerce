e3611867ae21bde9c3d1bd2b0620333630349071
Jairth <rodriguez@Chocofer.>
2024-11-01 20:52:43 -0500

    Commit de prueba

--- src/app/shared/components/social/social.component.ts
+++ src/app/shared/components/social/social.component.ts
@@ -6,8 +6,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [],
   templateUrl: './social.component.html',
   styleUrl: './social.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SocialComponent {
-
-}
+export class SocialComponent {}
